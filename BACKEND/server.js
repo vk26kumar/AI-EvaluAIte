333fa527d6d340a29018a402691c25a9c72bac60
Vishal Kumar <vkumar26062003@gmail.com>
2025-03-31 12:23:23 +0530

    Updatedddd

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -18,7 +18,12 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"], 
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use(
